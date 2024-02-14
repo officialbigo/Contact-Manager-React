32d28a1db8dfe56c1e839aad483ebf6c97c6a084
officialbigo <official.bigo.acc@gmail.com>
2024-02-14 07:13:39 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -28,14 +28,11 @@ function App() {
     if (authToken !== "") {
       localStorage.setItem("authToken", JSON.stringify(authToken));
       // console.log(authToken);
-      Axios.get(
-        "https://contact-manager-backend-z9p3.onrender.com/api/users/user",
-        {
-          headers: {
-            Authorization: `Bearer ${authToken}`,
-          },
-        }
-      )
+      Axios.get("/api/users/user", {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      })
         .then((res) => {
           const user_id = res.data.id;
           const user_email = res.data.email;
@@ -45,14 +42,11 @@ function App() {
         .catch((err) => {
           alert(err);
         });
-      Axios.get(
-        "https://contact-manager-backend-z9p3.onrender.com/api/contacts",
-        {
-          headers: {
-            Authorization: `Bearer ${authToken}`,
-          },
-        }
-      )
+      Axios.get("/api/contacts", {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      })
         .then((res) => {
           let i = 0;
           const initialContacts = [];
@@ -96,15 +90,11 @@ function App() {
     }
   };
   const addContactHandler = (contact) => {
-    Axios.post(
-      "https://contact-manager-backend-z9p3.onrender.com/api/contacts",
-      contact,
-      {
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-        },
-      }
-    )
+    Axios.post("/api/contacts", contact, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    })
       .then((res) => {
         const id = res.data._id;
         const { name, number, email } = res.data;
@@ -116,14 +106,11 @@ function App() {
   };
 
   const deleteContactHandler = (id) => {
-    Axios.delete(
-      `https://contact-manager-backend-z9p3.onrender.com/api/contacts/${id}`,
-      {
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-        },
-      }
-    ).catch((err) => {
+    Axios.delete(`/api/contacts/${id}`, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    }).catch((err) => {
       alert(err);
     });
     const new_contacts = contacts.filter((contact) => {
@@ -137,15 +124,11 @@ function App() {
       number: updatedContact.number,
       email: updatedContact.email,
     };
-    Axios.put(
-      `https://contact-manager-backend-z9p3.onrender.com/api/contacts/${updatedContact.id}`,
-      inputContact,
-      {
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-        },
-      }
-    )
+    Axios.put(`/api/contacts/${updatedContact.id}`, inputContact, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    })
       .then((res) => {
         const id = res.data._id;
         const { name, number, email } = res.data;
--- src/components/RegisterUser.js
+++ src/components/RegisterUser.js
@@ -17,10 +17,7 @@ const RegisterUser = () => {
   // localhost:5001/api/users/register
   const handleSubmit = (e) => {
     e.preventDefault();
-    Axios.post(
-      "https://contact-manager-backend-z9p3.onrender.com/api/users/register",
-      formData
-    )
+    Axios.post("/api/users/register", formData)
       .then((res) => {
         alert(`New user ${res.data.name} created.`);
         setFormData({
